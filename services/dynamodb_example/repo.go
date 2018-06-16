a1cb0d1b3a89fa0a6adcd37c5deed297a81a41b7
Steven Lee <kynrai@gmail.com>
2018-06-16 15:52:56 +0100

    Use base dynamodb package (#12)
    Use the base dynamoDB package instead of a wrapper. Add an example of
    using the dynamoDB batch APIs

--- services/dynamodb_example/repo.go
+++ services/dynamodb_example/repo.go
@@ -3,8 +3,10 @@ package dynamodb_example
 import (
 	"context"
 
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/kynrai/lilith/services/dynamodb_example/models"
-	"github.com/kynrai/lilith/storage/dynamodb"
 )
 
 const tableName = "things"
@@ -13,6 +15,7 @@ var _ Repo = (*repo)(nil)
 
 type Repo interface {
 	Getter
+	MultiGetter
 	Putter
 }
 
@@ -28,6 +31,17 @@ func (f GetterFunc) Get(ctx context.Context, id string) (*models.Thing, error) {
 }
 
 type (
+	MultiGetter interface {
+		GetMulti(ctx context.Context, ids ...string) ([]*models.Thing, error)
+	}
+	MultiGetterFunc func(ctx context.Context, ids ...string) ([]*models.Thing, error)
+)
+
+func (f MultiGetterFunc) GetMulti(ctx context.Context, ids ...string) ([]*models.Thing, error) {
+	return f(ctx, ids...)
+}
+
+type (
 	Putter interface {
 		Put(ctx context.Context, t *models.Thing) error
 	}
@@ -39,22 +53,52 @@ func (f PutterFunc) Put(ctx context.Context, t *models.Thing) error {
 }
 
 type repo struct {
-	db dynamodb.Repo
+	db dynamodb.DynamoDB
 }
 
-func New(db dynamodb.Repo) *repo {
+func New(db dynamodb.DynamoDB) *repo {
 	return &repo{db}
 }
 
 func (r *repo) Get(ctx context.Context, id string) (*models.Thing, error) {
-	var key struct {
-		ID string
+	t := models.Thing{}
+	getResp, err := r.db.GetItemWithContext(ctx, &dynamodb.GetItemInput{
+		Key: map[string]*dynamodb.AttributeValue{
+			"id": &dynamodb.AttributeValue{
+				S: aws.String(id),
+			},
+		},
+		TableName: aws.String("tableName"),
+	})
+	if err != nil {
+		return nil, err
 	}
-	key.ID = id
-	t := &models.Thing{}
-	return t, r.db.Get(ctx, tableName, &key, t)
+	return &t, dynamodbattribute.UnmarshalMap(getResp.Item, &t)
+}
+
+func (r *repo) GetMulti(ctx context.Context, ids ...string) ([]*models.Thing, error) {
+	keys := make([]map[string]*dynamodb.AttributeValue, 0, len(ids))
+	for _, v := range ids {
+		keys = append(keys, map[string]*dynamodb.AttributeValue{
+			"id": &dynamodb.AttributeValue{
+				S: aws.String(v),
+			},
+		})
+	}
+	getResps, err := r.db.BatchGetItemWithContext(ctx, &dynamodb.BatchGetItemInput{
+		RequestItems: map[string]*dynamodb.KeysAndAttributes{
+			tableName: &dynamodb.KeysAndAttributes{
+				Keys: keys,
+			},
+		},
+	})
+	if err != nil {
+		return nil, err
+	}
+	var things []*models.Thing
+	return things, dynamodbattribute.UnmarshalListOfMaps(getResps.Responses[tableName], &things)
 }
 
 func (r *repo) Put(ctx context.Context, t *models.Thing) error {
-	return r.db.Put(ctx, tableName, t)
+	return nil
 }
